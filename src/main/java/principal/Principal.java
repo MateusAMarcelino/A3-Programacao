f79fb0de63740b8800089287ece812b213b0550d
mitsuo <guihonda10@gmail.com>
2024-05-14 09:20:53 -0300

    carga inicial

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package principal;
 
-/**
- *
- * @author Mateus
- */
+import visao.FrmMenuPrincipal;
+
 public class Principal {
     
+    public static void main (String [] args){
+    
+    //Cria o objeto "tela"    
+    FrmMenuPrincipal tela = new FrmMenuPrincipal();
+    
+    //Método para exibir o objeto tela
+    tela.setVisible (true);
+    
+    }
 }
