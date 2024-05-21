67ebafe65f5e7e9b7d06a072f3ae21ba4738c65b
1072419633 <1072419633@UNS07DT106670.animaedu.intranet>
2024-05-21 11:37:06 -0300

    integrando o banco de dados

--- src/main/java/dao/AmigoDAO.java
+++ src/main/java/dao/AmigoDAO.java
@@ -1,30 +1,171 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import modelo.Amigo;
 
 public class AmigoDAO {
 
     //Cria ArrayList de Amigos:
-    public static ArrayList<Amigo> ListaAmigo = new ArrayList<>();
+    public ArrayList<Amigo> ListaAmigo = new ArrayList<>();
 
-    //Métodos Getter e Setter da ArrayList Amigos
-    public static ArrayList<Amigo> getListaAmigo() {
+     public ArrayList<Amigo> getListaAmigo() {
+
+        ListaAmigo.clear(); // Limpa nosso ArrayList
+        
+       //adicionando objeto amigo ao banco de dados
+        try {
+            Statement stmt = this.getConexao().createStatement();
+            ResultSet res = stmt.executeQuery("SELECT * FROM tb_amigos");
+            while (res.next()) {
+
+                int id = res.getInt("id");
+                String nome = res.getString("nome");
+                int telefone = res.getInt("telefone");
+                String email = res.getString("email");
+
+                Amigo objeto = new Amigo(id, nome, telefone, email);
+
+                ListaAmigo.add(objeto);
+            }
+            stmt.close();
+
+        } catch (SQLException ex) {
+            System.out.println("Erro:" + ex);
+        }
         return ListaAmigo;
     }
 
-    public static void setListaAmigo(ArrayList<Amigo> ListaAmigo) {
-        AmigoDAO.ListaAmigo = ListaAmigo;
+    public void setListaAmigo(ArrayList<Amigo> ListaAmigo) {
+        this.ListaAmigo = ListaAmigo;
     }
 
     //Método para buscar maior ID
-    public static int maiorID() {
+    public int maiorID() {
         int maiorID = 0;
-        for (int i = 0; i < ListaAmigo.size(); i++) {
-            if (ListaAmigo.get(i).getId() > maiorID) {
-                maiorID = ListaAmigo.get(i).getId();
-            }
+        try {
+            Statement stmt = this.getConexao().createStatement();
+            ResultSet res = stmt.executeQuery("SELECT MAX(id) id FROM tb_amigos");
+            res.next();
+            maiorID = res.getInt("id");
+            stmt.close();
+        } catch (SQLException ex) {
+            System.out.println("Erro:" + ex);
         }
         return maiorID;
+    }
+    
+    //conexão com o banco de dados 
+    public Connection getConexao() {
+
+        Connection connection = null;  //instância da conexão
+        try {
+            // Carregamento do JDBC Driver
+            String driver = "com.mysql.cj.jdbc.Driver";
+            Class.forName(driver);
+
+            // Configurar a conexão
+            String server = "localhost"; //caminho do MySQL
+            String database = "tb_amigos";
+            String url = "jdbc:mysql://" + server + ":3306/" + database + "?useTimezone=true&serverTimezone=UTC";
+            String user = "root";
+            String password = "root";
+
+            connection = DriverManager.getConnection(url, user, password);
+            // Testando..
+            if (connection != null) {
+                System.out.println("Status: Conectado!");
+            } else {
+                System.out.println("Status: NÃO CONECTADO!");
+            }
+            return connection;
+
+        } catch (ClassNotFoundException e) {  //Driver não encontrado
+            System.out.println("O driver nao foi encontrado. " + e.getMessage());
+            return null;
+        } catch (SQLException e) {
+            System.out.println("Nao foi possivel conectar...");
+            return null;
+        }
+    }
+    
+    public boolean insertAmigoBD(Amigo objeto) {
+        String sql = "INSERT INTO tb_amigos(id,nome,telefone,email) VALUES(?,?,?,?)";
+        try {
+            PreparedStatement stmt = this.getConexao().prepareStatement(sql);
+
+            stmt.setInt(1, objeto.getId());
+            stmt.setString(2, objeto.getNome());
+            stmt.setInt(3, objeto.getTelefone());
+            stmt.setString(4, objeto.getEmail());
+
+            stmt.execute();
+            stmt.close();
+
+            return true;
+        } catch (SQLException erro) {
+            System.out.println("Erro:" + erro);
+            throw new RuntimeException(erro);
+        }
+    }
+    
+    public boolean deleteAmigoBD(int id) {
+        try {
+            Statement stmt = this.getConexao().createStatement();
+            stmt.executeUpdate("DELETE FROM tb_amigos WHERE id = " + id);
+            stmt.close();
+
+        } catch (SQLException erro) {
+            System.out.println("Erro:" + erro);
+        }
+        return true;
+    }
+    
+    public boolean updateAmigoBD(Amigo objeto) {
+
+        String sql = "UPDATE tb_amigos set nome = ? ,telefone = ? ,email = ? , WHERE id = ?";
+
+        try {
+            PreparedStatement stmt = this.getConexao().prepareStatement(sql);
+
+            stmt.setString(1, objeto.getNome());
+            stmt.setInt(2, objeto.getTelefone());
+            stmt.setString(3, objeto.getEmail());
+            stmt.setInt(4, objeto.getId());
+
+            stmt.execute();
+            stmt.close();
+
+            return true;
+
+        } catch (SQLException erro) {
+            System.out.println("Erro:" + erro);
+            throw new RuntimeException(erro);
+        }
+    }
+
+    public Amigo carregaAmigo(int id) {
+        Amigo objeto = new Amigo();
+        objeto.setId(id);
+        try {
+            Statement stmt = this.getConexao().createStatement();
+
+            ResultSet res = stmt.executeQuery("SELECT * FROM tb_amigos WHERE id = " + id);
+            res.next();
+
+            objeto.setNome(res.getString("nome"));
+            objeto.setTelefone(res.getInt("telefone"));
+            objeto.setEmail(res.getString("email"));
+
+            stmt.close();
+        } catch (SQLException erro) {
+            System.out.println("Erro:" + erro);
+        }
+        return objeto;
     }
 }
