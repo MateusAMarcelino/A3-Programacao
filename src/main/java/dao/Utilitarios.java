cd55f572e321d78386d79adfefac96912a8057a4
lorenzoeid <lorenzocolaferro12@gmail.com>
2024-05-28 10:30:04 -0300

    feat; criação de uma classe de utilitarios

---
+++ src/main/java/dao/Utilitarios.java
@@ -0,0 +1,43 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Utilitarios {
+    
+    //conexão com o banco de dados 
+    public Connection getConexao() {
+        
+        Connection connection = null;  //instância da conexão
+        try {
+            // Carregamento do JDBC Driver
+            String driver = "com.mysql.cj.jdbc.Driver";
+            Class.forName(driver);
+
+            // Configurar a conexão
+            String server = "localhost"; //caminho do MySQL
+            String database = "db_a3";
+            String url = "jdbc:mysql://" + server + ":3306/" + database + "?serverTimezone=UTC";
+            String user = "root";
+            String password = "root";
+
+            connection = DriverManager.getConnection(url, user, password);
+            // Testando..
+            if (connection != null) {
+                System.out.println("Status: Conectado!");
+            } else {
+                System.out.println("Status: NÃO CONECTADO!");
+            }
+            return connection;
+
+        } catch (ClassNotFoundException e) {  //Driver não encontrado
+            System.out.println("O driver nao foi encontrado. " + e.getMessage());
+            return null;
+        } catch (SQLException e) {
+            System.out.println("Nao foi possivel conectar...");
+            return null;
+        }
+    }
+}
