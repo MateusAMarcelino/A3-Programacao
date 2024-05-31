53b7e0f97a177b2d60ce29064c04a7633d00b63b
Mateus Aloisio Marcelino <MateusA.Marcelino@gmail.com>
2024-05-31 16:35:41 -0300

    docs ; colocado duas linhas de inserção para cada tabela

--- banco.sql
+++ banco.sql
@@ -34,3 +34,16 @@ CREATE TABLE tb_emprestimos (
     DataEmprestimo DATE NULL,
     PRIMARY KEY(idEmprestimo)
 );
+
+
+# inserindo amigos na BD
+INSERT INTO tb_amigos(id,nome,telefone,email) VALUES (1,"Mateus","123456789","emaildomateus@gmail.com");
+INSERT INTO tb_amigos(id,nome,telefone,email) VALUES (2,"Maria","987654321","emaildamaria@gmail.com");
+
+# inserindo ferramentas na BD
+INSERT INTO tb_ferramentas(IdFerramentas,NomeFerramentas,MarcaFerramentas,CustoFerramentas,DisponibilidadeFerramentas) VALUES (1,"Cortador de Grama","Tramontina","199.99", true);
+INSERT INTO tb_ferramentas(IdFerramentas,NomeFerramentas,MarcaFerramentas,CustoFerramentas,DisponibilidadeFerramentas) VALUES (2,"Martelo","Vonder","59.99", true);
+
+# inserindo emprestimos na BD
+INSERT INTO tb_emprestimos(IdEmprestimo,NomeAmigo,IdFerramentas,DataEmprestimo) VALUES (1,NULL,NULL,NULL);
+INSERT INTO tb_emprestimos(IdEmprestimo,NomeAmigo,IdFerramentas,DataEmprestimo) VALUES (2,NULL,NULL,NULL);
