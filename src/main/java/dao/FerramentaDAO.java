dcebf68c50a9af700770f858cc35e424346b65ce
Mateus Aloisio Marcelino <MateusA.Marcelino@gmail.com>
2024-06-03 12:14:58 -0300

    style : comentei a classe FerramentaDAO

--- src/main/java/dao/FerramentaDAO.java
+++ src/main/java/dao/FerramentaDAO.java
@@ -9,13 +9,19 @@ import modelo.Ferramenta;
 
 public class FerramentaDAO {
      
-    //chamando o metodo getConexao
+    
     private Utilitario ut = new Utilitario();
     
+    /**
+     * Cria um ArrayList para as ferramentas.
+     */
     public static ArrayList<Ferramenta> ListaFerramentas = new ArrayList<>();
     
     
-     
+     /**
+      * Coleta as informações das ferramentas do banco de dados e cria um objeto com essas informações no ArrayList da ListaFerramentas.
+      * @return A lista de ferramentas após pegar os dados.
+      */
      public ArrayList<Ferramenta> getListaFerramentas(){
          ListaFerramentas.clear();
          try {
@@ -42,6 +48,10 @@ public class FerramentaDAO {
          FerramentaDAO.ListaFerramentas = ListaFerramentas;
      }
      
+     /**
+      * Procura o maior ID entre as ferramentas, buscando por todas as ferramentas no banco de dados.
+      * @return O maior ID encontrado.
+      */
      public int MaiorIdFerramentas(){
          int MaiorIdFerramentas = 0;
          try {
@@ -55,6 +65,12 @@ public class FerramentaDAO {
             }
          return MaiorIdFerramentas;
      }
+     
+     /**
+      * Insere as informações como uma ferramenta no banco de dados.
+      * @param ferramenta é o objeto ferramenta, que carrega as informações, como ID, Nome etc..
+      * @return True, caso seja possivel inserir, ou retorna um erro caso não seja possivel.
+      */
      public boolean InsertFerramentaDB(Ferramenta ferramenta){
          String res = "insert into tb_ferramentas(IdFerramentas,NomeFerramentas,MarcaFerramentas,CustoFerramentas,DisponibilidadeFerramentas) values (?,?,?,?,?)";
          try {
@@ -72,6 +88,12 @@ public class FerramentaDAO {
              throw new RuntimeException(erro);
          }
      }
+     
+     /**
+      * Atualiza as informações de uma ferramenta já existente no banco de dados.
+      * @param ferramenta é o objeto da ferramenta, o qual será atualizado.
+      * @return True, caso seja possivel atualizar, ou retorna um erro caso não seja possivel.
+      */
      public boolean UpdateFerramentaDB(Ferramenta ferramenta){
          String res = "update tb_ferramentas set IdFerramentas=?,NomeFerramentas=?,MarcaFerramentas=?,CustoFerramentas=?,DisponibilidadeFerramentas=? where IdFerramentas=?";
          try {
@@ -91,6 +113,11 @@ public class FerramentaDAO {
          }
              
          }
+     /**
+      * Deleta uma ferramenta do banco de dados.
+      * @param IdFerramentas É o ID da ferramenta que deve ser deletada.
+      * @return True, caso seja possivel deletar, ou retorna um erro caso não seja possivel.
+      */
      public boolean DeleteFerramentaDB(int IdFerramentas){
          try {
              Statement smt = ut.getConexao().createStatement();
