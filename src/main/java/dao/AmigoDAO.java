10c077c61c56b818c1abf66e41efa824ea09bc87
Mateus Aloisio Marcelino <MateusA.Marcelino@gmail.com>
2024-06-04 14:07:15 -0300

    style : comentada a classe AmigoDAO

--- src/main/java/dao/AmigoDAO.java
+++ src/main/java/dao/AmigoDAO.java
@@ -11,14 +11,21 @@ public class AmigoDAO {
 
     private Utilitario ut = new Utilitario();
     
-    //Cria ArrayList de Amigos:
+    /**
+     * Cria a ArrayList para os amigos.
+     */
     public ArrayList<Amigo> ListaAmigo = new ArrayList<>();
-
+    
+    
+    /**
+     * Coleta as informações dos amigos do banco de dados e cria um objeto com essas informações no ArrayList da ListaAmigos. 
+     * @return A lista de ferramentas após pegar os dados.
+     */
      public ArrayList<Amigo> getListaAmigo() {
 
         ListaAmigo.clear(); // Limpa nosso ArrayList
-        
-       //adicionando objeto amigo ao banco de dados
+                 
+       
         try {
             Statement stmt = ut.getConexao().createStatement();
             ResultSet res = stmt.executeQuery("SELECT * FROM tb_amigos");
@@ -45,7 +52,10 @@ public class AmigoDAO {
         this.ListaAmigo = ListaAmigo;
     }
 
-    //Método para buscar maior ID
+   /**
+    * Procura o maior ID entre os amigos, buscando por todos os amigos no banco de dados.
+    * @return O maior ID encontrado.
+    */
     public int maiorID() {
         int maiorID = 0;
         try {
@@ -60,6 +70,11 @@ public class AmigoDAO {
         return maiorID;
     }
     
+    /**
+     * Insere as informações como um amigo no banco de dados.
+     * @param objeto é o objeto de amigos, que carrega as informações, como id,nome, etc..
+     * @return True, caso seja possivel inserir o amigo, ou retorna um erro caso não seja possivel.
+     */
     public boolean insertAmigoBD(Amigo objeto) {
         String sql = "INSERT INTO tb_amigos(id,nome,telefone,email) VALUES(?,?,?,?)";
         try {
@@ -80,6 +95,11 @@ public class AmigoDAO {
         }
     }
     
+    /**
+     * Deleta um amigo do banco de dados.
+     * @param id é o id do amigo que será deletado.
+     * @return True, caso seja possivel deletar, ou retorna um erro caso não seja possivel.
+     */
     public boolean deleteAmigoBD(int id) {
         try {
             Statement stmt = ut.getConexao().createStatement();
@@ -92,6 +112,11 @@ public class AmigoDAO {
         return true;
     }
     
+    /**
+     * Atualiza as informações de um amigo, já existente no banco de dados.
+     * @param objeto é o objeto do amigo, o qual será atualizado.
+     * @return True, caso seja possivel atualizar, ou retorna um erro caso não seja possivel.
+     */
     public boolean updateAmigoBD(Amigo objeto) {
 
         String sql = "UPDATE tb_amigos set nome = ? ,telefone = ? ,email = ?  WHERE id = ?";
@@ -114,7 +139,12 @@ public class AmigoDAO {
             throw new RuntimeException(erro);
         }
     }
-
+/**
+ * Carrega o objeto amigo, com as informações, coletando as informações do banco de dados
+ * e preenchendo os valores, definindo no objeto.
+ * @param id é o Id que será carregado.
+ * @return 
+ */
     public Amigo carregaAmigo(int id) {
         Amigo objeto = new Amigo();
         objeto.setId(id);
